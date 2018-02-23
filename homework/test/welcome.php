b2735dd6e59f23448d76bf0f1856c5cb79513238
gekkoga <35320491+gekkoga@users.noreply.github.com>
2018-02-23 12:50:33 +0700

    Add files via upload

---
+++ homework/test/welcome.php
@@ -0,0 +1,18 @@
+<html>
+<body bgcolor="black">
+<title>Welcome</title>
+<link rel="stylesheet" a href="script/style.css">
+<link rel="icon" size="any" mask href="pics/hitoha.ico">
+<font color="white">
+
+<nav class="w3-bar w3-blue-gray"> 
+<a href="index.html" class="w3-button w3-bar-item" >Home</a>
+</nav>
+<p class="w3-justify w3-center">
+welcome <br>
+<?php
+
+ echo $_POST['name']; ?>
+</p>
+</body>
+</html>
